f76cf85b3291cf1719650030dd247dd41ee35afb
chain <klaus.mika1052@gmail.com>
2023-07-22 21:13:30 -0300

    change header to be fixed and change bg color if cliente scrolls down

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,37 @@
-import Image from 'next/image'
-import logo from '../assets/logo.svg'
-import Link from 'next/link'
+'use client'
+
 import { NavBar } from './NavBar'
+import { Logo } from './Logo'
+import { useEffect, useState } from 'react'
 
 export function Header() {
-  return (
-    <header className="flex items-center justify-between">
-      <Link href={'/'}>
-        <Image src={logo} alt="logo do Robust Habit" className="max-sm:w-24" />
-      </Link>
+  const [distance, setDistance] = useState(0)
 
+  function handleScroll() {
+    const topElement = document.querySelector('#top')
+
+    if (topElement) {
+      const { top } = topElement.getBoundingClientRect()
+      setDistance(top)
+    }
+  }
+
+  useEffect(() => {
+    if (window.innerWidth < 768) {
+      window.addEventListener('scroll', handleScroll)
+      return () => {
+        window.removeEventListener('scroll', handleScroll)
+      }
+    }
+  }, [])
+
+  return (
+    <header
+      className={`flex items-center justify-between w-full pt-4 max-md:px-4 max-md:fixed max-md:-ml-4 max-md:h-[65px] max-md:pt-0 max-md:z-50${
+        distance < -60 ? ' max-md:backdrop-blur-md max-md:bg-white/30' : ''
+      } `}
+    >
+      <Logo />
       <NavBar />
     </header>
   )
