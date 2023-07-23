37c40f623fd7805e67ba5c21c888b33d7e02f723
chain <klaus.mika1052@gmail.com>
2023-07-22 21:10:12 -0300

    fix ref bug

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,9 @@
-import { ButtonHTMLAttributes, ReactElement, ReactNode } from 'react'
+import {
+  ButtonHTMLAttributes,
+  ReactElement,
+  ReactNode,
+  forwardRef,
+} from 'react'
 import { twMerge } from 'tailwind-merge'
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
@@ -6,17 +11,22 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   icon?: ReactElement
 }
 
-export function Button({ children, icon, ...rest }: ButtonProps) {
-  return (
-    <button
-      {...rest}
-      className={twMerge(
-        'font-title-secondary text-2xl bg-secondary w-[9.5rem] h-11 rounded-tr-[2rem] flex gap-3 items-center justify-center',
-        rest.className,
-      )}
-    >
-      {children}
-      {!!icon && icon}
-    </button>
-  )
-}
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ children, icon, ...rest }: ButtonProps, ref) => {
+    return (
+      <button
+        ref={ref}
+        {...rest}
+        className={twMerge(
+          'font-title-secondary text-2xl bg-secondary w-[9.5rem] h-11 rounded-tr-[2rem] flex gap-3 items-center justify-center',
+          rest.className,
+        )}
+      >
+        {children}
+        {!!icon && icon}
+      </button>
+    )
+  },
+)
+
+Button.displayName = 'Button'
