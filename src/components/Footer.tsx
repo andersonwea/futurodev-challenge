3a753b0a7ed0005f0666b579d02615e56573fcb1
chain <klaus.mika1052@gmail.com>
2023-07-22 21:08:19 -0300

    create footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,77 @@
+import { Container } from './Container'
+import { Logo } from './Logo'
+import { Text } from '@/components/Text'
+import { SubHead } from './SubHead'
+import { NavLink } from './NavLink'
+
+import instagram1 from '../assets/instagram/instagram1.jpg'
+import instagram2 from '../assets/instagram/instagram2.jpg'
+import instagram3 from '../assets/instagram/instagram3.jpg'
+import instagram4 from '../assets/instagram/instagram4.jpg'
+import instagram5 from '../assets/instagram/instagram5.jpg'
+import instagram6 from '../assets/instagram/instagram6.jpg'
+import Image from 'next/image'
+
+export function Footer() {
+  const instagramPosts = [
+    instagram1,
+    instagram2,
+    instagram3,
+    instagram4,
+    instagram5,
+    instagram6,
+  ]
+
+  return (
+    <footer className="pt-12 pb-4 max-sm:pt-6">
+      <Container className="grid grid-cols-[3fr_1fr_2fr] gap-5 max-sm:grid-cols-1 max-sm:space-y-4">
+        <div>
+          <Logo />
+          <div className="mt-9 max-sm:mt-4">
+            <Text>143 your coffee shop address goes here, New York</Text>
+            <Text>Call: +00 123 456 789</Text>
+            <Text>Email: info@robust-habit.com</Text>
+            <Text>Web: www.robust-habit.com</Text>
+          </div>
+        </div>
+
+        <div>
+          <SubHead subhead className="text-3xl">
+            links
+          </SubHead>
+          <nav className="mt-9 flex flex-col gap-4 pt-4 max-sm:flex-row max-sm:mt-0 max-sm:items-center">
+            <NavLink href="#about">sobre</NavLink>
+            <NavLink href="#services">serviços</NavLink>
+            <NavLink href="#prices">preços</NavLink>
+            <NavLink href="#equipments">contato</NavLink>
+          </nav>
+        </div>
+
+        <div>
+          <SubHead subhead className="text-3xl">
+            veja no instagram
+          </SubHead>
+          <div className="mt-9 flex flex-wrap gap-2 pt-4 max-w-[335px] max-sm:mt-0">
+            {instagramPosts.map((instagramPost, i) => {
+              return (
+                <a
+                  href="https://instagram.com"
+                  target="_blank"
+                  key={'postagem' + i}
+                  rel="noreferrer"
+                >
+                  <Image
+                    src={instagramPost}
+                    alt="postagem do instagram"
+                    width={105}
+                    height={105}
+                  />
+                </a>
+              )
+            })}
+          </div>
+        </div>
+      </Container>
+    </footer>
+  )
+}
